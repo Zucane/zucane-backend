0e458958b3c33f8ba8d0542082edd22149b9b881
MiguelSP040 <20213tn040@utez.edu.mx>
2025-10-30 11:08:05 -0600

    fix: refactor config, app

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 import sys
 import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+import config
+from pydantic import BaseModel
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder
+from stellar_sdk.exceptions import BadRequestError, BadResponseError, NotFoundError
+import mysql.connector
+from mysql.connector import Error as MySQLError
+from fastapi import HTTPException
 
 # Agregar src al path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
@@ -145,7 +155,261 @@ async def health_check(db: Session = Depends(get_db)):
             "version": "1.0.0"
         }
 
+class PurchaseRequest(BaseModel):
+    asset_amount: float
+    business_private_key: str
 
+class GetBalanceRequest(BaseModel):
+    account_public_key: str
+
+class IssueRequest(BaseModel):
+    amount: float
+    issuer_private_key: str
+
+def get_db_connection():
+    """Establishes a connection to the MySQL database."""
+    try:
+        # NEW: Ahora te conectas a tu propia base de datos local
+        connection = mysql.connector.connect(
+            host="192.168.100.8",          # <-- CAMBIO: 'localhost' es tu propia PC
+            user="root",  # <-- CAMBIO: El usuario local que creaste
+            password="root", # <-- CAMBIO: La clave local que creaste
+            database="zucane_db"    # <-- CAMBIO: El nombre de tu DB local
+        )
+        return connection
+    except MySQLError as e:
+        print(f"Error connecting to MySQL Database: {e}")
+        return None
+
+def send_payment(source_keypair, receiver_public_key, amount:str, asset = Asset.native()):
+    source_public_key = source_keypair.public_key
+    source_account = server.load_account(source_public_key)
+
+    base_fee = 100
+    transaction = (
+        TransactionBuilder(
+            source_account=source_account,
+            network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
+            base_fee=base_fee,
+        )
+        .add_text_memo("Sending payment ->")  # Add a memo
+        .append_payment_op(receiver_public_key, asset, amount)
+        .set_timeout(30)  # Make this transaction valid for the next 30 seconds only
+        .build()
+    )
+
+    transaction.sign(source_keypair)
+    response = server.submit_transaction(transaction)
+
+    return response
+
+# --- API ENDPOINTS ---
+
+@app.get("/")
+def read_root():
+    return {"HACKATHON2025UTEZ": "XOCHI", "status": "online"}
+
+@app.post("/api/business/purchase")
+def purchase(request: PurchaseRequest):
+    business_keypair = Keypair.from_secret(request.business_private_key)
+    treasury_keypair = Keypair.from_secret(settings.TREASURY_S)
+
+    xlm_to_send = request.asset_amount * settings.ZUCOIN_PRICE_IN_XLM
+
+    try:
+        send_payment(source_keypair= business_keypair, receiver_public_key=settings.TREASURY_P,
+                     amount = str(xlm_to_send))
+    except (BadRequestError, BadResponseError) as e:
+        print("TRANSACTION FAILED AT SENDING PAYMENT")
+        print(e)
+        raise HTTPException(status_code=400, detail=f"Stellar error when sending XLM: {e}")
+    except Exception as e:
+        print("UNEXPECTED ERROR AT SENDING PAYMENT")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+
+
+    try:
+        asset = Asset(settings.ASSET_NAME, settings.ISSUER_P)
+        response = send_payment(source_keypair = treasury_keypair, receiver_public_key=business_keypair.public_key,
+                     asset =asset,amount = str(request.asset_amount))
+
+    except (BadRequestError, BadResponseError) as e:
+        print("TRANSACTION FAILED AT SENDING TOKENS")
+        print(e)
+        raise HTTPException(status_code=400, detail=f"Error when sending {settings.ASSET_NAME}: {e}")
+    except Exception as e:
+        print("UNEXPECTED ERROR AT SENDING TOKENS")
+        print(e)
+        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+
+    id_transaction_stellar = response['hash']
+
+    asset_to_XLM = request.asset_amount * settings.ZUCOIN_PRICE_IN_XLM
+    asset_to_MXN = asset_to_XLM * settings.XLM_TO_MXN_RATE
+    pdf_path = generate_invoice_pdf.generate_pdf(settings.ASSET_NAME,
+                                                 request.asset_amount, asset_to_MXN, id_transaction_stellar)
+
+    if not os.path.exists(pdf_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+        # 2. Devuelve el archivo
+    return FileResponse(
+        path=pdf_path,
+        media_type="application/pdf",
+        filename=f"purchase_invoice.pdf"
+    )
+
+@app.get("/api/account/balance")
+def account_balance(account_public_key: str):
+    asset_balance = 0
+
+    try:
+        account_data = server.accounts().account_id(account_public_key).call()
+        for balance in account_data['balances']:
+            if not balance['asset_type'] == "native" and balance['asset_code'] == settings.ASSET_NAME:
+                asset_balance = balance['balance']
+
+        asset_balance = float(asset_balance)
+        asset_to_XLM = asset_balance * settings.ZUCOIN_PRICE_IN_XLM
+        asset_to_MXN = asset_to_XLM * settings.XLM_TO_MXN_RATE
+        hectares = asset_balance * settings.ZUCOIN_TO_HECTARE_RATIO
+        return {"asset_balance": asset_balance, "asset_to_XLM": asset_to_XLM,
+                "asset_to_MXN": asset_to_MXN, "hectares": hectares}
+
+    except NotFoundError:
+        raise HTTPException(
+            status_code=404,
+            detail="Wallet hasn't been funded yet or it doesn't exist."
+        )
+
+    except BadRequestError:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid public key"
+        )
+
+    except (ConnectionError, Exception) as e:
+        print(e)
+        raise HTTPException(
+            status_code=500,
+            detail="Internal server error."
+        )
+
+@app.get("/api/asset/balance")
+def get_asset_balance():
+    asset_balance = 0
+    try:
+        asset_data = server.accounts().account_id(settings.TREASURY_P).call()
+        for balance in asset_data['balances']:
+            if balance['asset_type'] != "native" and balance['asset_code'] == settings.ASSET_NAME and balance['asset_issuer'] == settings.ISSUER_P:
+                asset_balance = balance['balance']
+
+        asset_balance = float(asset_balance)
+        return {"asset_balance": asset_balance}
+
+    except NotFoundError:
+        raise HTTPException(
+            status_code=404,
+            detail="Asset not found."
+        )
+
+    except BadRequestError:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid public key"
+        )
+
+    except (ConnectionError, Exception) as e:
+        print(e)
+        raise HTTPException(
+            status_code=500,
+            detail="Internal server error."
+        )
+
+@app.post("/api/government/issue")
+def issue_asset(request: IssueRequest):
+    try:
+        issuer_keypair = Keypair.from_secret(request.issuer_private_key)
+        treasury_public_key = settings.TREASURY_P
+        asset = Asset(settings.ASSET_NAME, settings.ISSUER_P)
+        response = send_payment(
+            source_keypair=issuer_keypair,
+            receiver_public_key=treasury_public_key,
+            asset=asset,
+            amount=str(request.amount),
+        )
+        return {
+            "status": "Assets issued successfully",
+            "newly_issued_amount": request.amount,
+            "destination_account": treasury_public_key,
+            "receipt_hash": response['hash']
+        }
+
+
+    except (BadRequestError, BadResponseError) as e:
+        print("TRANSACTION FAILED AT ISSUING TOKENS")
+        print(e)
+        if "op_no_trust" in str(e):
+            raise HTTPException(status_code=400,
+                                detail="Issuing Error: The Treasury account does not have a trustline for this asset.")
+
+        raise HTTPException(status_code=400, detail=f"Stellar error during issuance: {e}")
+
+    except Exception as e:
+        print(f"UNEXPECTED ERROR AT ISSUING TOKENS: {e}")
+        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+
+
+@app.get("/api/account/history",)
+def get_account_history(account_public_key: str ):
+    try:
+        response = server.payments().for_account(account_public_key).limit(4).order(desc=True).call()
+        records = response['_embedded']['records']
+
+        history = []
+        for record in records:
+            simplified_record = {
+                "id": record.get('id'),
+                "type": record.get('type'),
+                "created_at": record.get('created_at'),
+                "transaction_hash": record.get('transaction_hash'),
+            }
+
+            if record['type'] == 'payment':
+                simplified_record.update({
+                    "amount": record.get('amount'),
+                    "asset_code": record.get('asset_code', 'XLM'),
+                    "from": record.get('from'),
+                    "to": record.get('to'),
+                })
+            elif record['type'] == 'create_account':
+                simplified_record.update({
+                    "amount": record.get('starting_balance'),
+                    "asset_code": "XLM",
+                    "from": record.get('funder'),
+                    "to": record.get('account'),
+                })
+            history.append(simplified_record)
+
+        return history
+
+    except NotFoundError:
+        raise HTTPException(
+            status_code=4404,
+            detail="Wallet hasn't been funded yet or it doesn't exist."
+        )
+    except BadRequestError:
+        raise HTTPException(
+            status_code=404,
+            detail="Invalid public key"
+        )
+    except (ConnectionError, Exception) as e:
+        print(e)
+        raise HTTPException(
+            status_code=500,
+            detail="Internal server error."
+        )
+        
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
---
+++ src/invoice/invoice_generator.py
@@ -0,0 +1,205 @@
+import requests
+import json
+import pytz
+import locale
+from locale import getlocale
+from tzlocal import get_localzone
+
+from datetime import datetime
+
+
+class InvoiceClientConfig:
+    api_key: str
+    date_format: str
+    locale: str | None
+    timezone: str
+    endpoint_url: str
+
+    def __init__(
+        self,
+        api_key: str,
+        date_format: str = "%d %b %Y",
+        locale: str | None = getlocale()[0] or None,
+        timezone: str = str(get_localzone()),
+        endpoint_url: str = "https://invoice-generator.com",
+    ):
+        self.api_key = api_key
+        self.date_format = date_format
+        self.locale = locale
+        self.timezone = timezone
+        self.endpoint_url = endpoint_url
+
+
+class InvoiceGenerator:
+    """API Object for Invoice-Generator tool - https://invoice-generator.com/"""
+
+    # Below are the default template parameters that can be changed (see https://github.com/Invoiced/invoice-generator-api/)
+    TEMPLATE_PARAMETERS = [
+        "header",
+        "to_title",
+        "ship_to_title",
+        "invoice_number_title",
+        "date_title",
+        "payment_terms_title",
+        "due_date_title",
+        "purchase_order_title",
+        "quantity_header",
+        "item_header",
+        "unit_cost_header",
+        "amount_header",
+        "subtotal_title",
+        "discounts_title",
+        "tax_title",
+        "shipping_title",
+        "total_title",
+        "amount_paid_title",
+        "balance_title",
+        "terms_title",
+        "notes_title",
+    ]
+
+    def __init__(
+        self,
+        config: InvoiceClientConfig,
+        sender: str,
+        to: str,
+        logo: str | None = None,
+        ship_to: str | None = None,
+        number: int | None = None,
+        payments_terms: str | None = None,
+        due_date=None,
+        notes: str | None = None,
+        terms: str | None = None,
+        currency: str = "USD",
+        date: datetime | None = None,
+        discounts: float = 0,
+        tax: float = 0,
+        shipping: float = 0,
+        amount_paid: float = 0,
+    ):
+        """Object constructor"""
+        self.logo = logo
+        self.config = config
+        self.sender = sender
+        self.to = to
+        self.ship_to = ship_to
+        self.number = number
+        self.currency = currency
+        self.custom_fields = []
+        self.date = date or datetime.now(tz=pytz.timezone(self.config.timezone))
+        self.payment_terms = payments_terms
+        self.due_date: datetime | None = due_date
+        self.items = []
+        self.fields = {"tax": "%", "discounts": False, "shipping": False}
+        self.discounts = discounts
+        self.tax = tax
+        self.shipping = shipping
+        self.amount_paid = amount_paid
+        self.notes = notes
+        self.terms = terms
+        self.template = {}
+
+    def _to_json(self):
+        """
+        Parsing the object as JSON string
+        Please note we need also to replace the key sender to from, as per expected in the API but incompatible with from keyword inherent to Python
+        We are formatting here the correct dates
+        We are also resetting the two list of Objects items and custom_fields so that it can be JSON serializable
+        Finally, we are handling template customization with its dict
+        """
+        locale.setlocale(locale.LC_ALL, self.config.locale)
+        object_dict = self.__dict__.copy()
+        object_dict["from"] = object_dict.get("sender")
+        object_dict["date"] = self.date.strftime(self.config.date_format)
+        object_dict["due_date"] = (
+            self.due_date.strftime(self.config.date_format) if self.due_date else None
+        )
+        object_dict.pop("sender")
+        for index, item in enumerate(object_dict["items"]):
+            object_dict["items"][index] = item.__dict__
+        for index, custom_field in enumerate(object_dict["custom_fields"]):
+            object_dict["custom_fields"][index] = custom_field.__dict__
+        for template_parameter, value in self.template.items():
+            object_dict[template_parameter] = value
+        object_dict.pop("template")
+        object_dict.pop("config")
+        return json.dumps(object_dict)
+
+    def add_custom_field(self, name=None, value=None):
+        """Add a custom field to the invoice"""
+        self.custom_fields.append(CustomField(name=name, value=value))
+
+    def add_item(
+        self,
+        name: str | None = None,
+        quantity=0,
+        unit_cost=0.0,
+        description: str | None = None,
+    ):
+        """Add item to the invoice"""
+        self.items.append(
+            Item(
+                name=name,
+                quantity=quantity,
+                unit_cost=unit_cost,
+                description=description or "",
+            )
+        )
+
+    def download(self, file_path):
+        """Directly send the request and store the file on path"""
+        json_string = self._to_json()
+        headers = {
+            "Authorization": f"Bearer {self.config.api_key}",
+        }
+        if self.config.locale:
+            headers["Accept-Language"] = self.config.locale
+        response = requests.post(
+            self.config.endpoint_url,
+            json=json.loads(json_string),
+            stream=True,
+            headers=headers,
+        )
+        if response.status_code == 200:
+            open(file_path, "wb").write(response.content)
+        else:
+            raise Exception(
+                f"Invoice download request returned the following message:{response.json()} Response code = {response.status_code} "
+            )
+
+    def set_template_text(self, template_parameter, value):
+        """If you want to change a default value for customising your invoice template, call this method"""
+        if template_parameter in InvoiceGenerator.TEMPLATE_PARAMETERS:
+            self.template[template_parameter] = value
+        else:
+            raise ValueError(
+                "The parameter {} is not a valid template parameter. See docs.".format(
+                    template_parameter
+                )
+            )
+
+    def toggle_subtotal(self, tax="%", discounts=False, shipping=False):
+        """Toggle lines of subtotal"""
+        self.fields = {"tax": tax, "discounts": discounts, "shipping": shipping}
+
+
+class Item:
+    """Item object for an invoice"""
+
+    def __init__(self, name, quantity, unit_cost, description=""):
+        """Object constructor"""
+        self.name = name
+        self.quantity = quantity
+        self.unit_cost = unit_cost
+        self.description = description
+
+
+class CustomField:
+    """Custom Field object for an invoice"""
+
+    def __init__(self, name, value):
+        """Object constructor"""
+        self.name = name
+        self.value = value
+
+
